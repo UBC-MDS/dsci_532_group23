76fd6d21d1f36a0ecc6cd2b23c4ab898ac2b62bc
Jayme Gordon <jaymegordo@gmail.com>
2021-01-22 15:40:31 -0800

    change back to run func

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -9,16 +9,12 @@ from .. import data_processing as dp
 from ..__init__ import getlog
 
 log = getlog(__name__)
-log.warning('LOGGING WARNING TEST')
-log.error('LOGGING ERROR TEST')
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
+log.info('server set')
+
 df = dp.df_clean()
-
-log.warning('Everything init')
-
-print('PRINT TEST')
 
 @app.callback(
     Output('world_map', 'figure'),
@@ -43,50 +39,51 @@ def update_map_energy(energy_type, year, *args, **kw):
     print(energy_type, year)
     return ch.world_map(df=df, energy_type=energy_type, year=str(year))
 
-# def run(app):
+def run(app):
 
-# set default chart
-fig = ch.world_map(df=df, year='2018', energy_type='all_renewable')
+    # set default chart
+    fig = ch.world_map(df=df, year='2018', energy_type='all_renewable')
 
-# Energy type dropdown, using uniqe energy_type values
-energy_types = [{'label': val.replace('_', ' ').title(), 'value': val} for val in df.energy_type.unique()]
-energy_dropdown = dcc.Dropdown(
-        id='energy_dropdown',
-        options=energy_types,
-        value='total',
-        multi=False,
-        style={'width': '200px'})
-    
-year_slider = dcc.Slider(
-    id='year_slider',
-    min=1980,
-    max=2018,
-    marks={year: str(year) for year in range(1980, 2019, 2)}
-)
+    # Energy type dropdown, using uniqe energy_type values
+    energy_types = [{'label': val.replace('_', ' ').title(), 'value': val} for val in df.energy_type.unique()]
+    energy_dropdown = dcc.Dropdown(
+            id='energy_dropdown',
+            options=energy_types,
+            value='total',
+            multi=False,
+            style={'width': '200px'})
+        
+    year_slider = dcc.Slider(
+        id='year_slider',
+        min=1980,
+        max=2018,
+        marks={year: str(year) for year in range(1980, 2019, 2)}
+    )
 
-app.layout = html.Div([
-    html.Div([energy_dropdown], style={
-        # 'display': 'inline-block',
-        # 'position': 'absolute',
-        # 'right': 0,
-        # 'float': 'right',
-        # 'margin': '20px',
-        'margin-bottom': '20px',
-        'margin-left': 'auto',
-        'horizontal-align': 'right',
-        # 'border': '3px solid green',
-        }),
-    dcc.Graph(
-        id='world_map',
-        figure=fig),
-    html.Div([year_slider], style={'margin-top': '20px'}),
-    # html.Div(id='dd-output-container')
-])
+    app.layout = html.Div([
+        html.Div([energy_dropdown], style={
+            # 'display': 'inline-block',
+            # 'position': 'absolute',
+            # 'right': 0,
+            # 'float': 'right',
+            # 'margin': '20px',
+            'margin-bottom': '20px',
+            'margin-left': 'auto',
+            'horizontal-align': 'right',
+            # 'border': '3px solid green',
+            }),
+        dcc.Graph(
+            id='world_map',
+            figure=fig),
+        html.Div([year_slider], style={'margin-top': '20px'}),
+        # html.Div(id='dd-output-container')
+    ])
 
-    # return app
+    return app
     # app.run_server(debug=True)
 
 if __name__ == '__main__':
-    # app = run(app)
+    log.info('running main')
+    app = run(app)
     log.info(f'app layout: {app.layout}')
     app.run_server(debug=True)
