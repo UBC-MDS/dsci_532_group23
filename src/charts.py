9f27201d7849d016fc10fc4ca181329f3ed926e7
Jayme Gordon <jaymegordo@gmail.com>
2021-01-22 19:07:28 -0800

    Add bar and multi_plot funcs

--- src/charts.py
+++ src/charts.py
@@ -4,19 +4,60 @@ import plotly.offline as py
 import seaborn as sns
 from matplotlib import pyplot
 from plotly.subplots import make_subplots
+import ipywidgets as ipw
 
+def multi_plots(df, **kw):
+    """Return world_map with bar_top 10"""
 
-def world_map(df, year='2018', energy_type='all_renewable', **kw):
-    """Create main world map plot of energy consumption"""
+    # filter df, both use same data
+    df = filter_df(df, **kw)
+    fig2 = top_bar(df=df)
+    fig1 = world_map(df=df)
 
-    # filter 
-    # NOTE filtering everyting less than 1e9.. some small countries have weird per capita values
-    df = df.loc[year] \
+    return fig1, fig2
+
+def filter_df(df, energy_type='renewables', year='1980'):
+    """Filter df before sending to top_n and world_map"""
+    return df.loc[year] \
         .pipe(lambda df: df[
             (df.energy_type == energy_type) &
             (df.country_code != 'WORL') &
-            (df.energy_per_capita < 1e9)
-            ])
+            (df.energy_per_capita < 1e9)])
+
+def top_bar(df, energy_type='renewables', year='1980', n=10):
+
+    # filter top n, sort
+    df = df.sort_values(['energy_per_capita']) \
+        .iloc[-1 * n:, :]
+
+    trace = go.Bar(
+        x=df.energy_per_capita,
+        y=df.country,
+        orientation='h',
+        # color_continuous_scale='Viridis',
+        marker=dict(
+            # color='Viridis',
+            colorscale='Viridis'
+            )
+    )
+
+    fig = go.FigureWidget(data=[trace])
+
+    xaxis = dict(
+        title='BTU per Capita'
+    )
+
+    fig.update_layout(
+        xaxis=xaxis,
+        title_text = f'Top {n} Energy Consumers',
+        margin=dict(t=30, b=0, r=0, l=100),
+        width=400
+    )
+
+    return fig
+
+def world_map(df, year='1980', energy_type='renewables', **kw):
+    """Create main world map plot of energy consumption"""
 
     map_trace = go.Choropleth(
         locations=df.country_code,
@@ -24,52 +65,50 @@ def world_map(df, year='2018', energy_type='all_renewable', **kw):
         locationmode='ISO-3',
         colorscale='viridis',
         # reversescale=True
-        # colorbar_title = "Millions USD",
+        colorbar_title='BTU per Capita',
     )
 
-    fig = go.Figure(data=[map_trace])
+    fig = go.FigureWidget(data=[map_trace])
 
     fig.update_layout(
         title_text = 'World Energy Consumption',
         margin=dict(t=30, b=0, r=0, l=20),
+        yaxis=dict(title='BTU per Capita'),
+        width=800
         # geo_scope='usa', # limite map scope to USA
     )
 
     return fig
 
-def chart_single_country(df, country='Canada'):
+def single_country(df, country='Canada'):
+    """Return line chart for single country with full history and all energy types"""
     fig = go.Figure()
-
     df = df[df.country == country]
-
     x = df.index.to_timestamp()
 
     xaxis = dict(
         ticktext=df.index.strftime('%Y'),
         tickvals=x,
         tickangle=270,
-        showgrid=False,
-    )
+        showgrid=False)
 
-    # traces = []
     energy_types = ['coal', 'natural_gas', 'petroleum', 'nuclear', 'renewables']
+
+    # loop energy types and add traces
     for energy in energy_types:
         df2 = df[df.energy_type == energy]
 
         trace = go.Scatter(
             name=energy,
             x=df2.index.to_timestamp(),
-            y=df2.energy,
-        )
+            y=df2.energy)
 
         fig.add_trace(trace)
-
-    # fig.append_trace(traces)
 
     fig.update_layout(
         xaxis=xaxis,
         margin=dict(t=30, b=0, r=0, l=0),
-        title='Energy Consumption (Canada)',
+        title=f'Energy Consumption ({country})',
         yaxis=dict(
             title='Energy Consumption (Quad BTU)',
             showgrid=False
